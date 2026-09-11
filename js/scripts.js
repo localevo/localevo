b7c2806d00e2a7fa9512f3af202a598bc2c8bb8e
Bora Bogdan <bogdan.bora@localevo.ro>
2026-09-11 18:44:18 +0300

    modificari

--- js/scripts.js
+++ js/scripts.js
@@ -36,9 +36,11 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
+    if (typeof SimpleLightbox !== 'undefined') {
+       new SimpleLightbox({
+           elements: '#portfolio a.portfolio-box'
+        });
+    }
 
 
     // =====================================
